b572d67d0912213596c52dba1308d5e7274e5960
ayumi043 <ayumi043@gmail.com>
2013-05-27 15:19:44 +0800

    f

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -22,7 +22,7 @@ class Admin::SessionsController < ApplicationController
         @user.update_attributes(:username => params[:username], :password => params[:password])
         # flash[:notice] = "欢迎您， #{@user.username}"
         # redirect_to posts_path
-        format.html { redirect_to admin_url, notice: "欢迎您， #{@user.username}" }
+        format.html { redirect_to "/admin", notice: "欢迎您， #{@user.username}" }
       else
         binding.pry
         flash[:notice] = "登录失败，用户名，或密码错误！"
