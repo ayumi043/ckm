15dffeea536d705e037b4724ebda17a1ab9e73fd
ayumi043 <ayumi043@gmail.com>
2013-06-04 09:25:04 +0800

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
+ 
   def current_class(arg_controller_name)
     'class=active' if controller_name == arg_controller_name
   end
 
-  
+  def current_menu(arg_controller_name)
+    'class=select' if controller_name == arg_controller_name
+  end
+
 end
