1027a109e981fc722561afb82b1d4bd931502c15
ayumi043 <ayumi043@gmail.com>
2013-07-01 12:56:07 +0800

    add fixed

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,8 +4,7 @@ class ContactController < ApplicationController
 
   def index
     @abouts = About.where(:ty_id => [1,2,4], :state => 1).select("id, title")
-    @about = About.where(:ty_id => 1, :state => 1).first
-    # render "abouts/show"
+    @about = About.where(:ty_id => 1, :state => 1).at(2)
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
     'class=select' if controller_name == arg_controller_name || controller_name == arg_c.first
   end
 
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def keywords(page_keywords)
+    content_for(:keywords) { page_keywords }
+  end
+
+  def description(page_description)
+    content_for(:description) { page_description }
+  end
+
 end
